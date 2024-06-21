187efba5fa47af85dca214c6509d17b685bed831
nikita <nikita.bosire@student.moringaschool.com>
2024-06-21 18:48:26 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+let employee = {
+    name: 'Nikita',
+    role: 'Deejay',
+    age: 20
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value
+    };
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    
+    let clonedEmployee = { ...employee};
+    
+    delete clonedEmployee[key];
+    return clonedEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    
+    delete employee[key];
+    return employee;
+}
